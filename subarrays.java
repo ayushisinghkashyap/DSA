2783d6a37a5e91780bdea2a24ab5728bc456b936
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-02-10 19:46:19 +0530

    Java

---
+++ subarrays.java
@@ -0,0 +1,25 @@
+/**
+ * subarrays
+ */
+public class subarrays {
+    public static void printSubarray(int numbers[]){
+        int totalSum=0;
+        for(int i=0;i<numbers.length;i++){
+            int start =i;
+            for (int j=0;j<numbers.length;j++){
+                int end =j;
+                for(int k=start;k<=end;k++){
+                    System.out.println(numbers[k]+" ");
+                }
+                totalSum++;
+                //System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] ={2,4,6,8,10};
+        printSubarray(numbers);
+    }
+}
