eddae02248186c3cdde51cf103946648bf5b603b
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-03-05 02:03:12 +0530

    2Darray

--- Array/Matrices.java
+++ Array/Matrices.java
@@ -14,8 +14,8 @@ public class Matrices {
         }
       
         //output
-        for(int i=0;i<n;i++){
-            for (int j = 0; j < m; j++) {
+        for(int i=0; i<n ;i++){
+            for (int j=0; j<m; j++) {
                 System.out.print(matrix[i][j]+ " ");
             }
             System.out.println();
