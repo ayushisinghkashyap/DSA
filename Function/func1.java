2c6b03cfa6d7d086a7a3cc3d836d8563efab4f65
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-08-31 00:13:30 +0530

    Function

---
+++ Function/func1.java
@@ -0,0 +1,23 @@
+public class func1 {
+    public static void noReturn(int n){ 
+        System.out.println("No return needed. "+ n);
+    }  
+
+    public static int ReturnInteger(int n){
+        System.out.println("Integer Return Needed.");
+        return n;
+    }
+    public static String ReturnString(String a){
+        System.out.println("String Return Needed");
+        return a; //this should be last line in the function 
+    }
+    // Use any data type you want to return 
+
+    public static void main(String[] args) {
+        noReturn(1);
+        System.out.println(ReturnInteger(1));
+        System.out.println(ReturnString("String"));
+
+        //SOUT is used to print return value
+    }
+}
